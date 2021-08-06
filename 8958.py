9a2e241753e8ccf09183a865ec143d7cd9dd1a45
chae09945ss <stat_chae0@sookmyung.ac.kr>
2021-08-06 16:34:01 +0900

    ox퀴즈

--- 8958.py
+++ 8958.py
@@ -3,15 +3,9 @@ for i in range(a):
     cnt=0
     b=input()
     fnt=0
-    if b[0]=='O':
-        fnt=1
-        cnt+=1
-    for j in range(1,len(b)):
-        if fnt!=0 and b[j]=='O':
+    for j in range(len(b)):
+        if b[j]=='O':
             fnt+=1
-            cnt+=fnt
-        elif fnt==0 and b[j]=='O':
-            fnt=1
             cnt+=fnt
         else:   fnt=0
     print(cnt)
