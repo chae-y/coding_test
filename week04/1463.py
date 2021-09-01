ad85d609ff03f74d1050d6634f974a8d03dd840e
LCY <stat_chae0@sookmyung.ac.kr>
2021-09-01 10:38:54 +0900

    1로만들기

---
+++ week04/1463.py
@@ -0,0 +1,17 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+dp = [999999999] * (n+1)
+
+dp[n] = 0
+
+for i in range(n, 0, -1):
+    if i % 3 == 0:
+        dp[i // 3] = min(dp[i // 3], dp[i] + 1)
+    if i % 2 == 0:
+        dp[i // 2] = min(dp[i // 2], dp[i] + 1)
+    if i > 1:
+        dp[i -1] = min(dp[i - 1], dp[i] + 1)
+
+print(dp[1])
