7beb136c67c39ef921fddc41c75b6ef242fd4c2a
LCY <stat_chae0@sookmyung.ac.kr>
2021-10-11 17:49:36 +0900

    부족한 금액 계산하기

---
+++ cpp/pro_4.cpp
@@ -0,0 +1,9 @@
+using namespace std;
+
+long long solution(int price, int money, int count){
+    long long answer = -1;
+    answer = (long long)price*count*(count+1)/2-money;
+    if(answer<=0)
+        answer = 0;
+    return answer;
+}
