8d3d1d42cef7e3538f5f845bdf5f5d588f88b4c2
LCY <stat_chae0@sookmyung.ac.kr>
2021-10-12 19:33:54 +0900

    최고의 팀 만들기

---
+++ cpp/1633.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int player[1001][2];
+int dp[1001][16][16];
+int N;
+
+int dfs(int idx, int white, int black) {
+	if (white == 15 && black == 15) return 0;
+	if (idx == N) return 0;
+	if (dp[idx][white][black]) return dp[idx][white][black];
+	int ans = 0;
+	if (white < 15) {
+		ans = dfs(idx+1, white + 1, black) + player[idx][0];
+	}
+	if (black < 15) {
+		ans = max(ans, dfs(idx + 1, white, black+1) + player[idx][1]);
+	}
+	return dp[idx][white][black] = max(ans, dfs(idx + 1, white, black));
+}
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int idx = 0;
+	int w, b;
+	while (scanf("%d %d", &w, &b) == 2) {
+		player[idx][0] = w;
+		player[idx++][1] = b;
+	}
+
+	N = idx;
+	cout << dfs(0,0,0) << "\n";
+
+	return 0;
+}
