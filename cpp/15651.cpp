fdf41494a347562af4d85ca86d3bf184c2d5af83
LCY <stat_chae0@sookmyung.ac.kr>
2021-10-15 22:59:40 +0900

    N과M3

---
+++ cpp/15651.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+
+int sol(int i, string s, int count){
+    if(count == m){
+        cout<<s<<"\n";
+        return 0;
+    }
+    s+=" ";
+    for(int j=1; j<=n; j++){
+        sol(j, s+to_string(j), count+1);
+    }
+    return 0;
+}
+
+int main()
+{
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >>n>>m;
+
+    for(int i=1; i<=n; i++){
+        sol(i,to_string(i), 1);
+    }
+    return 0;
+}
