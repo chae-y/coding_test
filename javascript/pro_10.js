27f8217d433f4f760ff1dc44a1aa0f0258f14b5f
chae <stat_chae0@sookmyung.ac.kr>
2022-01-23 22:47:29 +0900

    징검다리건너기

--- javascript/pro_10.js
+++ javascript/pro_10.js
@@ -1,15 +1,25 @@
-function solution(n, money) {
-    var answer = 0;
-    money = money.sort((a,b)=>a-b);
-    let dp = new Array(n+1).fill(0);
-    dp[0] = 1;
-    for(let m of money){
-        for(let i=1; i<=n; i++){
-            if(i-m>=0){
-                dp[i] += dp[i-m];
-                dp[i] %= 1000000007;
+function solution(stones, k) {
+    let answer = 0;
+    if(stones.length<=k)    return Math.max.apply(null,stones);
+    let start = 0;
+    let end = 200000000;
+    while(start<=end){
+        let mid = parseInt((start+end)/2);
+        let cnt=0;
+        for(let i=0; i<stones.length; i++){
+            if(stones[i]-mid<0){
+                cnt++;
+                if(cnt>=k)  break;
+            }else{
+                cnt=0;
             }
         }
+        if(cnt>=k){
+            end = mid-1;
+        }else{
+            start = mid+1;
+            answer = mid;
+        }
     }
-    return dp[n]%=1000000007;
+    return answer;
 }
